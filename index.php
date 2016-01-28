6081b7ec981378c114dc0a5a996bd71fd67191cb
sonestarae <sonesta.rae@gmail.com>
2016-01-28 13:09:47 -0700

    index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="script.js"></script>
+    <title></title>
+</head>
+<body>
+<?php
+
+?>
+<div id='cssmenu'>
+    <ul>
+        <li class='active'><a href='index.php'>Home</a></li>
+        <li><a href='login.php'>Log In</a></li>
+        <li><a href='adduser.php'>Register</a></li>
+    </ul>
+</div>
+<p>Welcome. Please choose an item from the menu.</p>
+</body>
+</html>
